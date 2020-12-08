784fbcdc377a35c52defb73aaa63aecb8c8f9739
nodeAriful <ariful.node82@gmail.com>
2020-12-08 22:19:48 +0600

    added remainder method

--- Hello.js
+++ Hello.js
@@ -1,9 +1,13 @@
-function add(a,b){
-    return a+b
+function add(a, b) {
+    return a + b
 }
 
-function divided(a,b){
-    return a/b
+function divided(a, b) {
+    return a / b
+}
+
+function remainder(a, b) {
+    return a % b
 }
 
 console.log(divided(10, 5));
